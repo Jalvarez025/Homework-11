c80286d0f36fbf26acd444e20b4f2c2a66cdbe37
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-10-23 20:51:28 -0500

    Fetch working

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const notesData = require('./Develop/db/notes.json');
+const notesData = require('./db/notes.json');
 
 const PORT = 3001;
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/api/notes', (req, res) => res.json(notesData));
